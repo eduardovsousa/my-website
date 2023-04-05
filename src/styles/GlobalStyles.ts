adf7c433d75e52d8c0e6f3586984570cd5844e28
Eduardo Varela Sousa <eduardo.varela@outlook.com.br>
2023-04-05 20:33:03 -0300

    new bg

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -42,7 +42,7 @@ export const GlobalStyles = createGlobalStyle`
     background: var(--color-bg);
     color: var(--color-white);
     line-height: 1.7;
-    background-image: url("../../src/assets/images/bg-texture.png");
+    background-image: url("/src/assets/images/bg-texture.png");
   }
 
   button {
